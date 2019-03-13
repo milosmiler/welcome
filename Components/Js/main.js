cde8dbd5d13ab55c73b8d47c4f34ed37d8a0fe36
jairo villalobos <jairo.villalobos@lunave.com>
2019-03-13 16:33:18 -0600

    ajustes de header

--- Components/Js/main.js
+++ Components/Js/main.js
@@ -42,3 +42,18 @@ document.getElementById('toggle').addEventListener('click', e => {
 		headAcordion[i].classList.add('active');
 	}
 });
+
+let burger = document.querySelector(".burger"),
+	menu = document.querySelector('.menu'),
+	close = document.querySelector('.close'),
+	overlay = document.querySelector('.overlay');
+
+	burger.addEventListener("click", function(){
+		menu.className += ' open';
+		overlay.className += ' open';
+	});
+
+	close.addEventListener("click", function(){
+		menu.classList.remove('open');
+		overlay.classList.remove('open')
+	});
