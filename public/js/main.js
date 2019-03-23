a6682aa4b729c8606baa290b429119ae8951ec21
Jairo villalobos <breakjai@yahoo.com.mx>
2019-03-22 20:31:43 -0600

    ajustes generales

--- public/js/main.js
+++ public/js/main.js
@@ -91,23 +91,15 @@ $('.cerrar').click(function () {
   overlay.classList.remove('open');
 });
 
-$('.owl-carousel.owl-nosotros').owlCarousel({
-  loop: true,
-  dots: true,
-  items: 1,
-  margin: 10,
-  nav: false
-});
+// $('.owl-carousel.owl-nosotros').owlCarousel({
+//   loop: true,
+//   dots: true,
+//   items: 1,
+//   margin: 10,
+//   nav: false
+// });
 
-var dot = $('.owl-carousel.owl-nosotros .owl-dots .owl-dot');
-dot.each(function () {
-  var index = $(this).index() + 1;
-  if (index < 10) {
-    $(this).html('0').append(index);
-  } else {
-    $(this).html(index);
-  }
-});
+
 
 $('.owl-carousel.owl-valores').owlCarousel({
   dots: false,
@@ -133,10 +125,94 @@ $('.owl-carousel.owl-valores').owlCarousel({
  $('form').parsley();
 
 
-// $('nav').localScroll();
-// $('.item-col').localScroll();
-
 $('.item-Titulo').on('click', function () {
   $('.items-box-general').hide();
   $(this).next().slideToggle('active');
+});
+
+ var sync1 = $("#sync1");
+    var sync2 = $("#sync2");
+    var slidesPerPage = 1; //globaly define number of elements per page
+    var syncedSecondary = true;
+
+    sync1.owlCarousel({
+        items: 1,
+        slideSpeed: 2000,
+        nav: false,
+        margin: 1,
+        autoplay: false, 
+        dots: false,
+        loop: true,
+        responsiveRefreshRate: 200,
+    }).on('changed.owl.carousel', syncPosition);
+
+    sync2
+        .on('initialized.owl.carousel', function() {
+            sync2.find(".owl-item").eq(0).addClass("current");
+        })
+        .owlCarousel({
+            items: slidesPerPage,
+            dots: true,
+            nav: false,
+            smartSpeed: 200,
+            slideSpeed: 500,
+            slideBy: slidesPerPage, //alternatively you can slide by 1, this way the active slide will stick to the first item in the second carousel
+            responsiveRefreshRate: 100
+        }).on('changed.owl.carousel', syncPosition2);
+
+    function syncPosition(el) {
+        //if you set loop to false, you have to restore this next line
+        //var current = el.item.index;
+
+        //if you disable loop you have to comment this block
+        var count = el.item.count - 1;
+        var current = Math.round(el.item.index - (el.item.count / 2) - .5);
+
+        if (current < 0) {
+            current = count;
+        }
+        if (current > count) {
+            current = 0;
+        }
+
+        //end block
+
+        sync2
+            .find(".owl-item")
+            .removeClass("current")
+            .eq(current)
+            .addClass("current");
+        var onscreen = sync2.find('.owl-item.active').length - 1;
+        var start = sync2.find('.owl-item.active').first().index();
+        var end = sync2.find('.owl-item.active').last().index();
+
+        if (current > end) {
+            sync2.data('owl.carousel').to(current, 100, true);
+        }
+        if (current < start) {
+            sync2.data('owl.carousel').to(current - onscreen, 100, true);
+        }
+    }
+
+    function syncPosition2(el) {
+        if (syncedSecondary) {
+            var number = el.item.index;
+            sync1.data('owl.carousel').to(number, 100, true);
+        }
+    }
+
+    sync2.on("click", ".owl-item", function(e) {
+        e.preventDefault();
+        var number = $(this).index();
+        sync1.data('owl.carousel').to(number, 300, true);
+    });
+
+    var dot = $('#sync2 .owl-dots .owl-dot');
+      dot.each(function () {
+        var index = $(this).index() + 1;
+        if (index < 10) {
+          $(this).html('0').append(index);
+        } else {
+          $(this).html(index);
+        }
 });
